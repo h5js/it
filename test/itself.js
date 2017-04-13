2e44d4175069d8f432c8a78afcb1f094cecbd5c6
战哥 <zhanyuan.lizhy@antfin.com>
2017-04-13 20:46:42 +0800

    add callback support and timeout support.

--- test/itself.js
+++ test/itself.js
@@ -1,13 +1,31 @@
 it('it self:', function*(it){
-  it.should(it).be.function();
-  it.should(it()).be.Promise();
-  it.should(it(undefined)).be.undefined();
-  it.should(it(null)).be.null();
-  it.should(it(true)).be.true();
-  it.should(it(false)).be.false();
-  it.should(it(0)).be.Promise();
-  it.should(yield it(0,'OK')).equal('OK');
-  it.should(it.should).be.function();
-  it.should(it.sum).be.function();
-  it.sum();
+  yield it('it properties:', function*(it){
+    it.should(it).be.function();
+    it.should(it()).be.Promise();
+    it.should(it(undefined)).be.undefined();
+    it.should(it(null)).be.null();
+    it.should(it(true)).be.true();
+    it.should(it(false)).be.false();
+    it.should(it(0)).be.Promise();
+    it.should(yield it(0,'OK')).equal('OK');
+    it.should(it.should).be.function();
+    it.should(it.sum).be.function();
+
+    it.sum();
+  });
+
+  yield it('it(ms) with source code:', function*(){
+    it.begin
+    var x = 123, y;
+    yield it(0);
+    it.end;
+
+    it.should(x).equal(123);
+
+    it.begin
+    x = 123;
+    y = 456;
+    it.end
+    it.should(y).equal(456);
+  });
 });
